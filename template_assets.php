21a64ed1f9fc6194465959c088b9325d987e9f81
Guilherme Viana <81631546+gui-bvr@users.noreply.github.com>
2021-11-24 14:37:27 -0300

    Delete template_assets.php

--- template_assets.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<script src="https://kit.fontawesome.com/079c8de8d4.js" crossorigin="anonymous"></script>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Shippori+Antique&display=swap" rel="stylesheet">
-</head>
-</html>
