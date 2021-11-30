46d9b490a613587f7a42574ad995c96d2cd4ad5c
Guilherme Viana <81631546+gui-bvr@users.noreply.github.com>
2021-11-30 09:23:14 -0300

    Add files via upload

---
+++ SQL/crud.sql
@@ -0,0 +1,11 @@
+-- Estrutura da tabela `pessoas`
+
+DROP TABLE IF EXISTS `pessoas`;
+CREATE TABLE IF NOT EXISTS `pessoas` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(250) COLLATE utf8_bin NOT NULL,
+  `telefone` varchar(250) COLLATE utf8_bin NOT NULL,
+  `idade` int(250) NOT NULL,
+  `email` varchar(250) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=24 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
